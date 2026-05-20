299496efcb97fca7e07532f6cc9734e9be700979
Bikash Khanal <khanalbikash007@gmail.com>
2026-05-20 16:15:31 +0545

    mono repo partial push udated

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,9 @@
+
+export function formatDate(date : Date) : string {
+    return new Date(date).toLocaleDateString('en-US') 
+}
+
+export function slugify(text:string): string {
+    return text.toLowerCase().replace("/\s+/g", "_");
+}
+
